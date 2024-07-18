365ecff252b41604494a181fe5e69cb1f86cbb8f
Antimony26 <103689703+Antimony26@users.noreply.github.com>
2024-07-18 03:42:04 +0100

    functions for loading data in catalog.html, description.html

--- integrated-training-soln/src/main.js
+++ integrated-training-soln/src/main.js
@@ -4,6 +4,29 @@ import { loginUser, registerUser } from "./index.js";
 import "./main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const mockData = {
+  courses: [
+    {
+      id: 1,
+      image: "https://via.placeholder.com/150",
+      title: "Course 1",
+      description: "This is the description for Course 1.",
+    },
+    {
+      id: 2,
+      image: "https://via.placeholder.com/150",
+      title: "Course 2",
+      description: "This is the description for Course 2.",
+    },
+    {
+      id: 3,
+      image: "https://via.placeholder.com/150",
+      title: "Course 3",
+      description: "This is the description for Course 3.",
+    },
+  ],
+};
+
 document.addEventListener("DOMContentLoaded", async function () {
   console.log("DOM fully loaded and parsed");
 
@@ -86,36 +109,83 @@ function fetchUsername() {
   }
 }
 
+/* Generate dynamic content for course cards  */
 function createCourseCard(course) {
-  const template = document.getElementById("course-card-template");
-  if (!template) {
-    console.error("Template element not found");
-    return;
-  }
-  const card = template.content.cloneNode(true);
+  const card = document.createElement("div");
+  card.classList.add("col-md-4", "mb-4");
 
-  // Selecting elements inside the card
-  const img = card.querySelector(".card-img-top");
-  const title = card.querySelector(".card-title");
-  const description = card.querySelector(".card-text");
-  const link = card.querySelector(".btn.custom-btn");
+  card.innerHTML = `
+    <div class="card">
+      <img src="${course.image}" class="card-img-top" alt="Course Image" />
+      <div class="card-body">
+        <h5 class="card-title">${course.title}</h5>
+        <p class="card-text">${course.description}</p>
+        <a href="#" class="btn custom-btn course-link" data-course-id="${course.id}">Learn More</a>
+      </div>
+    </div>
+  `;
 
-  img.src = course.image || "../images/defaultImage.jpg";
-  img.alt = course.title;
-  title.textContent = course.title;
-  description.textContent = course.description;
-  link.href = `description.html?courseId=${course.id}`;
-
-  card.querySelector(".col.course-card").dataset.courseId = course.id; // Example dataset usage
-
-  // Filtering courses using search bar
-  card.querySelector(".col.course-card").dataset.title = course.title.toLowerCase();
+  const learnMoreLink = card.querySelector(".course-link");
+  learnMoreLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    const courseId = learnMoreLink.dataset.courseId;
+    viewCourse(courseId);
+  });
 
   return card;
 }
 
-function viewCourse(courseId) {
-  window.location.href = `description.html?courseId=${courseId}`;
+async function viewCourse(courseId) {
+  //   try {
+  //     const courseDetails = await fetchCourseDetails(courseId);
+  //     const url = `description.html?courseId=${courseDetails.id}`;
+  //     window.location.href = url;
+  //   } catch (error) {
+  //     console.error("Error fetching course details:", error);
+  //   }
+  console.log("Viewing course with ID:", courseId);
+  console.log("mockData:", mockData);
+
+  try {
+    const courseDetails = await fetchCourseDetails(courseId);
+    const url = `description.html?courseId=${courseDetails.id}`;
+    window.location.href = url;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function loadCourseDescription() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const courseId = urlParams.get("courseId");
+
+  try {
+    const course = await fetchCourseDetails(courseId); // API call here
+    renderCourseDetails(course);
+  } catch (error) {
+    console.error("Error loading course description:", error);
+  }
+}
+
+function renderCourseDetails(course) {
+  const courseNameElement = document.querySelector(".header-text");
+  const courseDescriptionElement = document.querySelector(".col-7 p");
+  const courseImageElement = document.querySelector(".col-5 img");
+  const courseSubtextElement = document.querySelector(".desc-text p");
+
+  if (courseNameElement) {
+    courseNameElement.textContent = course.title;
+  }
+  if (courseDescriptionElement) {
+    courseDescriptionElement.textContent = course.description;
+  }
+  if (courseImageElement) {
+    courseImageElement.src = course.image;
+    courseImageElement.alt = course.title;
+  }
+  if (courseSubtextElement) {
+    courseSubtextElement.textContent = course.subtitle;
+  }
 }
 
 async function loadUserCourses() {
@@ -123,10 +193,6 @@ async function loadUserCourses() {
 }
 
 async function loadCourseCatalog() {
-  // Implementation here
-}
-
-async function loadCourseDescription() {
   // Implementation here
 }
 
@@ -140,28 +206,6 @@ function filterCourses() {
 
 function loadMockCourseCatalog() {
   console.log("Inside loadMockCourseCatalog function");
-  const mockData = {
-    courses: [
-      {
-        id: 1,
-        image: "https://via.placeholder.com/150",
-        title: "Course 1",
-        description: "This is the description for Course 1.",
-      },
-      {
-        id: 2,
-        image: "https://via.placeholder.com/150",
-        title: "Course 2",
-        description: "This is the description for Course 2.",
-      },
-      {
-        id: 3,
-        image: "https://via.placeholder.com/150",
-        title: "Course 3",
-        description: "This is the description for Course 3.",
-      },
-    ],
-  };
   const catalogContainer = document.getElementById("catalog-section");
   if (!catalogContainer) {
     console.error("Catalog section not found");
@@ -172,5 +216,48 @@ function loadMockCourseCatalog() {
   mockData.courses.forEach((course) => {
     const courseCard = createCourseCard(course);
     catalogContainer.appendChild(courseCard);
+  });
+}
+
+/* API calls  */
+async function fetchCourses() {
+  try {
+    const response = await fetch("https://api.example.com/courses"); // Replace with your actual API endpoint
+    const courses = await response.json();
+
+    const container = document.getElementById("courses-container");
+
+    courses.forEach((course) => {
+      const courseCard = createCourseCard(course);
+      container.appendChild(courseCard);
+    });
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+  }
+}
+
+async function fetchCourseDetails(courseId) {
+  // // Replace with  API endpoint and  API call
+  // const apiUrl = `https://api.example.com/courses/${courseId}`;
+  // const response = await fetch(apiUrl);
+  // if (!response.ok) {
+  //   throw new Error(`HTTP error! Status: ${response.status}`);
+  // }
+  // return await response.json();
+  console.log(`Fetching course details for courseId: ${courseId}`);
+  console.log("mockData:", mockData);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const course = mockData.courses.find((course) => course.id === courseId);
+      if (!course) {
+        const errorMessage = `Course details not found for courseId: ${courseId}`;
+        console.error(errorMessage);
+        reject(new Error(errorMessage));
+      } else {
+        console.log(`Course details found for courseId ${courseId}:`, course);
+        resolve(course);
+      }
+    }, 5000); // Simulate 1 second delay (remove in real API implementation)
   });
 }
---
+++ integrated-training-soln/webpack.config.cjs
@@ -0,0 +1,110 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : "style-loader";
+
+const config = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    assetModuleFilename: "images/[name][ext]",
+    publicPath: "/",
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    hot: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/login.html",
+      filename: "login.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/description.html",
+      filename: "description.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/dashboard.html",
+      filename: "dashboard.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/course.html",
+      filename: "course.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/catalog.html",
+      filename: "catalog.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/about.html",
+      filename: "about.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]", // Maintain original file names and place them in 'images' folder
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader", // Use html-loader for processing HTML files
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".*", ".js", ".mjs", ".css"],
+    alias: {
+      // xyz$: path.resolve(__dirname, "path/to/file.js"),
+      "@images": path.resolve(__dirname, "src/images"),
+      "@styles": path.resolve(__dirname, "src/main.css"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+    },
+    modules: [path.resolve(__dirname, "src")],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
