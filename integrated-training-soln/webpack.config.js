2749a24911c9257704d5c56bd1f8bf647a0d6391
Antimony26 <103689703+Antimony26@users.noreply.github.com>
2024-07-19 13:35:56 +0100

    adding clean-webpack-plugin

--- integrated-training-soln/webpack.config.js
+++ integrated-training-soln/webpack.config.js
@@ -3,6 +3,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -25,6 +26,7 @@ const config = {
     historyApiFallback: true,
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
